45b1c4648ccccb5d08a19e45629945ea7b51cee6
Kirill Sidorov <kirill.sidorov.94@bk.ru>
2019-08-24 06:14:41 +0300

    ThreeC

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -22,4 +22,33 @@ describe "Check get url" do
     page.status_code.should be 200
   end
 
+  it "get posts_list" do
+    visit "/users"
+    fill_in 'evaluation', with: 3
+    fill_in 'limit', with: 1000
+    click_button "Получить N по средней оценке постов"
+    page.status_code.should be 200
+  end
+
+  it "get page new post" do
+    visit "/users/2/posts/new"
+    expect(page).to have_content "Новый пост"
+  end
+
+  it "create_new_post" do
+    visit "/users/2/posts/new"
+    fill_in 'post[title]', with: 'Новый пост'
+    fill_in 'post[description]', with: 'Текст нового поста'
+    click_button "Create Post"
+    page.status_code.should be 200
+  end
+
+  it "check validation create new post" do
+    visit "/users/2/posts/new"
+    fill_in 'post[title]', with: ''
+    fill_in 'post[description]', with: ''
+    click_button "Create Post"
+    page.status_code.should be 422
+  end
+
 end
